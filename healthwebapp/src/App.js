4167dad575fd95545a9b9b68f6bea0bc6c943594
Steven Edeson <34163003+ComSciStevenEdeson@users.noreply.github.com>
2020-02-07 18:55:54 +0000

    Create page template for main pages identified
    Imported react-router-dom
    
    Created template pages and created paths to access the pages in apps.js

--- healthwebapp/src/App.js
+++ healthwebapp/src/App.js
@@ -2,8 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter, Route,Switch} from "react-router-dom";
+import {Login} from "./Webpages/login";
+import {PatientHome} from "./Webpages/patienthome";
+import {DoctorHome} from "./Webpages/doctorhome";
+import {PatientDetails} from "./Webpages/patientdetails";
+import {PatientExercise} from "./Webpages/patientexercise";
+import {PatientFoodIntake} from "./Webpages/patientfoodintake";
+import {PatientList} from "./Webpages/patientlist"
+import {Register} from "./Webpages/register";
+
 function App() {
   return (
+    <BrowserRouter>
+    <Switch>
+     <Route path='/' exact component ={Login} />
+     <Route path='/Patient/Homepage' exact component ={PatientHome} />
+     <Route path='/Patient/Food-Intake' exact component ={PatientFoodIntake} />
+     <Route path='/Patient/Exercise' exact component ={PatientExercise} />
+     <Route path='/Patient/MyDetails' exact component ={PatientDetails} /> {/* need to pass in patient ID */}
+     <Route path='/HealthCareProfessional/Homepage' exact component ={DoctorHome} />
+     <Route path='/HealthCareProfessional/My-Patient-List' exact component ={PatientList} />
+     <Route path='/HealthCareProfessional/Patient-Details' exact component ={PatientDetails} />{/* need to pass in patient ID */}
+     <Route path='/Register' exact component ={Register} />
+      <Route path='/' render={() => <div> Error 404: page not found :(</div>}/>
+    </Switch>
+    </BrowserRouter>
+    /*
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,7 +44,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div>*/
   );
 }
 
---
+++ healthwebapp/src/Webpages/patienthome.js
@@ -0,0 +1,8 @@
+import * as React from "react";
+
+//interface Props{}
+
+export const PatientHome = () => {
+
+    return <div>  Patient Home Page  </div>
+};

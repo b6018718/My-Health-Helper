f5b4187eba2035df24dec096554657d6bedc9efd
Anthony Dranfield <34162989+b6018718@users.noreply.github.com>
2020-02-10 22:15:27 +0000

    Added log on functionality
    Followed tutorial on https://serverless-stack.com/ to create a log on system. No back end functionality has been implemented yet

--- healthwebapp/src/App.js
+++ healthwebapp/src/App.js
@@ -1,50 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './css/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { BrowserRouter, Route,Switch} from "react-router-dom";
-import {Login} from "./Webpages/login";
-import {PatientHome} from "./Webpages/patienthome";
-import {DoctorHome} from "./Webpages/doctorhome";
-import {PatientDetails} from "./Webpages/patientdetails";
-import {PatientExercise} from "./Webpages/patientexercise";
-import {PatientFoodIntake} from "./Webpages/patientfoodintake";
-import {PatientList} from "./Webpages/patientlist"
-import {Register} from "./Webpages/register";
+import Routes from './components/routes'
 
-function App() {
+function App(props) {
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
   return (
-    <BrowserRouter>
-    <Switch>
-     <Route path='/' exact component ={Login} />
-     <Route path='/Patient/Homepage' exact component ={PatientHome} />
-     <Route path='/Patient/Food-Intake' exact component ={PatientFoodIntake} />
-     <Route path='/Patient/Exercise' exact component ={PatientExercise} />
-     <Route path='/Patient/MyDetails' exact component ={PatientDetails} /> {/* need to pass in patient ID */}
-     <Route path='/HealthCareProfessional/Homepage' exact component ={DoctorHome} />
-     <Route path='/HealthCareProfessional/My-Patient-List' exact component ={PatientList} />
-     <Route path='/HealthCareProfessional/Patient-Details' exact component ={PatientDetails} />{/* need to pass in patient ID */}
-     <Route path='/Register' exact component ={Register} />
-     <Route path='/' render={() => <div> Error 404: page not found :(</div>}/>
-    </Switch>
-    </BrowserRouter>
-    /*
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>*/
+      <Routes appProps={{ isAuthenticated, userHasAuthenticated }} />
+    </div>
   );
 }
 
---
+++ healthwebapp/src/components/routes.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import { BrowserRouter, Route,Switch} from "react-router-dom";
+import Login from "../Webpages/login";
+import {PatientHome} from "../Webpages/patienthome";
+import {DoctorHome} from "../Webpages/doctorhome";
+import {PatientDetails} from "../Webpages/patientdetails";
+import {PatientExercise} from "../Webpages/patientexercise";
+import {PatientFoodIntake} from "../Webpages/patientfoodintake";
+import {PatientList} from "../Webpages/patientlist"
+import {Register} from "../Webpages/register";
+import AppliedRoute from "./appliedRoute";
+import AuthenticedRoute from "./authenticedRoute";
+import UnauthenticatedRoute from "./unauthenticedRoute";
+
+function Routes({ appProps }) {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <UnauthenticatedRoute path='/' exact component ={Login} appProps={{ appProps }} />
+                <AuthenticedRoute path='/Patient/Homepage' exact component ={PatientHome} appProps={{ appProps }} />
+                <AuthenticedRoute path='/Patient/Food-Intake' exact component ={PatientFoodIntake} appProps={{ appProps }} />
+                <AuthenticedRoute path='/Patient/Exercise' exact component ={PatientExercise} appProps={{ appProps }} />
+                <AuthenticedRoute path='/Patient/MyDetails' exact component ={PatientDetails} appProps={{ appProps }} />
+                <AuthenticedRoute path='/HealthCareProfessional/Homepage' exact component ={DoctorHome} appProps={{ appProps }} />
+                <AuthenticedRoute path='/HealthCareProfessional/My-Patient-List' exact component ={PatientList} appProps={{ appProps }} />
+                <AuthenticedRoute path='/HealthCareProfessional/Patient-Details' exact component ={PatientDetails} appProps={{ appProps }} />
+                <AuthenticedRoute path='/Register' exact component ={Register} appProps={{ appProps }} />
+
+                <Route path='/' render={() => <div> Error 404: page not found :(</div>} />
+            </Switch>
+        </BrowserRouter>
+    );
+  }
+  
+  export default Routes;
+  

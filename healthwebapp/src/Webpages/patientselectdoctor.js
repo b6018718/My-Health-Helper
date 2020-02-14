d348339ade2f85b06bf3855a7df75ce035c2aa61
Steven Edeson <34163003+ComSciStevenEdeson@users.noreply.github.com>
2020-02-14 21:02:16 +0000

    Finished layout design for select doctor
    Created function that can create X number of elements. TODO: apply functionally to list of doctors fetched from the database

--- healthwebapp/src/Webpages/patientselectdoctor.js
+++ healthwebapp/src/Webpages/patientselectdoctor.js
@@ -6,11 +6,28 @@ import {Button, Form, Col, Row} from "react-bootstrap";
 import '../css/PatientSelectDoctor.css'
 import '../css/Login.css';
 import '../css/Register.css';
+import OverflowScrolling from 'react-overflow-scrolling'
+import * as ScrollArea from "react-scrollbar";
 
 export default function Register(props){
 
-
-
+    
+    function addDoctorList(doctorList)
+    {
+        var i;
+        var buttonArray =[];
+        for (i = 0; i < 100; i++){
+        buttonArray.push(addButtonToList("New doctor: " + i.toString()));
+        }
+        return buttonArray;
+    }
+    function addButtonToList(btnText, inc)
+    {
+        var button = (<button type="button" key={inc} className="list-group-item list-group-item-action">{btnText}</button>)
+        return button;
+        //document.getElementById("doctorList").appendChild(button);
+    }
+    const doctorList = addDoctorList("Test")
     function handleSubmit(event){
         // Log in system designed around code from https://serverless-stack.com/chapters/redirect-on-login.html
 
@@ -19,10 +36,22 @@ export default function Register(props){
         console.log(props.appProps.nAccEmail)
         console.log(props.appProps.nAccPassword)
             //props.history.push('/Patient/Homepage');
-    }
+    }//"list-group-item list-group-item-action active"
+    /*        <button type="button" class="list-group-item list-group-item-action active">
+    Cras justo odio
+  </button>
+  <button type="button" class="list-group-item list-group-item-action">Dapibus ac facilisis in</button> */
 return(
     <div className = "selectDoctor">
-selectDoctor here
+        <br></br>
+         <div className="Title">Please select your doctor from the list below:</div>
+
+        <div className = "list-group">
+
+            {doctorList}
+            </div>
+ 
+         <br></br>
 
 <Button className="registerButton" variant="primary" type="submit" onClick={handleSubmit} value="patient">
                     Register as Patient

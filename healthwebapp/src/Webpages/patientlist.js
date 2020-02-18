c2ec4a7ee075e04dc77039eefab2ff50be342373
Steven Edeson <34163003+ComSciStevenEdeson@users.noreply.github.com>
2020-02-18 17:32:01 +0000

    Clicking on a patient takes doctor to patient details page
    Currently the patient details page is just a template, and the patient id is just logged to console. Once patient details page is created, we will need to pass the patient id into it from the patient list.

--- healthwebapp/src/Webpages/patientlist.js
+++ healthwebapp/src/Webpages/patientlist.js
@@ -43,7 +43,7 @@ function SelectPatientDetailsWithoutSocket(props){
         if (patients.length == 0)
         {
             console.log("Went into no patients message");
-            buttonArray.push(<p className="Paragraph">No patients have selected you as their registered docotor. If you believe any of your patients are using this app, and that they should be sharing their data with you, please tell your patient to select you as their registered doctor for their account.</p>)
+            buttonArray.push(<p className="Paragraph">No patients have selected you as their registered doctor. If you believe any of your patients are using this app, and that they should be sharing their data with you, please tell your patient to select you as their registered doctor for their account.</p>)
         }else
         {
             for(let patient of patients){
@@ -56,10 +56,17 @@ function SelectPatientDetailsWithoutSocket(props){
 
     function addButtonToList(patient, inc)
     {
-        var button = (<button type="button" key={inc} //onClick={doctorClicked} 
+        var button = (<button type="button" key={inc} onClick={patientClicked} 
         value={patient._id} className="list-group-item list-group-item-action">{`${patient.forename} ${patient.surname}`}</button>)
         return button;
         //document.getElementById("doctorList").appendChild(button);
+    }
+
+    function patientClicked(event){
+        var button = event.target;
+        //setSelectedId(button.value);
+        console.log(button.value);//TODO pass to patient details/record page
+        props.history.push('/HealthCareProfessional/Patient-Details');
     }
 
     return (

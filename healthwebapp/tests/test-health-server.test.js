852ec77cd23330c4ebb3d4e15fb899cbaeb7e698
Zak Rooley <33326861+rooleyzee123@users.noreply.github.com>
2020-03-03 15:12:55 +0000

    Upto remove dr working

--- healthwebapp/tests/test-health-server.test.js
+++ healthwebapp/tests/test-health-server.test.js
@@ -4,144 +4,161 @@ server = require('../../healthwebserver/server.js')
 var socketURL = 'http://localhost:5000';
 
 
-var options ={
-    transports: ['websocket'],
-    'force new connection': true
-//https://github.com/liamks/Testing-Socket.IO/blob/master/test/test-chat-server.js
-//http://liamkaufman.com/blog/2012/01/28/testing-socketio-with-mocha-should-and-socketio-client/
+var options = {
+  transports: ['websocket'],
+  'force new connection': true
+  //https://github.com/liamks/Testing-Socket.IO/blob/master/test/test-chat-server.js
+  //http://liamkaufman.com/blog/2012/01/28/testing-socketio-with-mocha-should-and-socketio-client/
 };
 
-var newDoctortest = { 
-email: 'rooleyzee123@gmail.com',
-password: 'Test',
-forename: 'Zak',
-surname: 'Rooley',
-doctor: true };
-
-var newPatienttest = { 
+var newDoctortest = {
   email: 'rooleyzee123@gmail.com',
   password: 'Test',
   forename: 'Zak',
   surname: 'Rooley',
-  doctor: true };
+  doctor: true
+};
 
-describe("Web Health app Server",function(){
+var DrLogin = {
+  email: 'rooleyzee123@gmail.com',
+  password: 'Test'
+}
 
-let socket_1;
-beforeEach((done) =>{
-socket_1 = io.connect(socketURL,
-{forceNew: true, query: ''});
-socket_1.on ('connect',()=>{
-done();
-});
-});
+var DrLoginNoPass = {
+  email: 'rooleyzee123@gmail.com',
+  password: ''
+}
 
-afterEach((done)=>
-{
-socket_1 && socket_1.connected && socket_1.disconnect();
-done();
-});
+var DrLoginPassincorrect = {
+  email: 'rooleyzee123@gmail.com',
+  password: ''
+}
 
+var newPatienttest = {
+  email: 'rooleyzee123@gmail.com',
+  password: 'Test',
+  forename: 'Zak',
+  surname: 'Rooley',
+  doctor: true
+};
 
-//TEST one
-it('Connection to server', done => {
-  if(socket_1.connected)
-  {
+describe("Web Health app Server", function () {
+
+  let socket_1;
+  beforeEach((done) => {
+    socket_1 = io.connect(socketURL,
+      { forceNew: true, query: '' });
+    socket_1.on('connect', () => {
+      done();
+    });
+  });
+
+  afterEach((done) => {
+    socket_1 && socket_1.connected && socket_1.disconnect();
     done();
-  }
-    
-});
+  });
 
 
-//TEST two make a new doctor account
-  it('Make new doctor',done =>
-  {
-    socket_1.on('logInResult',function(data){
-if(data.message = "message: success")
-{done();}
+  //TEST one
+  it('Connection to server', done => {
+    if (socket_1.connected) {
+      done();
+    }
+
+  });
+
+
+  //TEST two make a new doctor account
+  it('Make new doctor', done => {
+    socket_1.on('logInResult', function (data) {
+      if (data.message = "message: success") { done(); }
     })
-  socket_1.emit('signUp',newDoctortest);
-  
+    socket_1.emit('signUp', newDoctortest);
   });
 
+  //TEST Doctor Login
+  it('Doctor Login', done => {
+    socket_1.on('logInResult', function (data) {
 
-  //TEST Make a new patient account
-  it('Make new patient',done =>
-  {
-  socket_1.emit('signUp',newUsertest);
-  done();
+      if (data.result) { done(); }
+    })
+    socket_1.emit('logIn', DrLogin);
   });
+
   //TEST remove doctor account
-  it('Make new patient',done =>
-  {
-  socket_1.emit('signUp',newUsertest);
-  done();
+  it('Remove Doctor account', done => {
+    socket_1.on('deleteAccountResults', function (data) {
+      if (data = "success") { done(); }
+    })
+    socket_1.emit('logIn', DrLogin);
+    socket_1.on('logInResult', function (data) {
+
+      socket_1.emit('deleteAccount', {});
+    })
+    
   });
 
   //TEST remove patient account
-  it('Make new patient',done =>
-  {
-  socket_1.emit('signUp',newUsertest);
-  done();
+  it('Make new patient', done => {
+    socket_1.emit('signUp', newUsertest);
+    done();
+  });
+  //TEST Make a new patient account
+  it('Make new patient', done => {
+    socket_1.emit('signUp', newUsertest);
+    done();
   });
 
+
   //TEST Add food 
-  it('Make new patient',done =>
-  {
-  socket_1.emit('signUp',newUsertest);
-  done();
+  it('Make new patient', done => {
+    socket_1.emit('signUp', newUsertest);
+    done();
   });
 
   //TEST add exersise
-  it('Make new patient',done =>
-  {
-  socket_1.emit('signUp',newUsertest);
-  done();
+  it('Make new patient', done => {
+    socket_1.emit('signUp', newUsertest);
+    done();
   });
 
   //TEST add to blood sugar list
-  it('Make new patient',done =>
-  {
-  socket_1.emit('signUp',newUsertest);
-  done();
+  it('Make new patient', done => {
+    socket_1.emit('signUp', newUsertest);
+    done();
   });
 
 
   //TEST check blood sugar list
-  it('Make new patient',done =>
-  {
-  socket_1.emit('signUp',newUsertest);
-  done();
+  it('Make new patient', done => {
+    socket_1.emit('signUp', newUsertest);
+    done();
   });
 
 
   //TEST Change assigned doctor
-  it('Make new patient',done =>
-  {
-  socket_1.emit('signUp',newUsertest);
-  done();
+  it('Make new patient', done => {
+    socket_1.emit('signUp', newUsertest);
+    done();
   });
 
 
   //TEST Get my doctor
-  it('Make new patient',done =>
-  {
-  socket_1.emit('signUp',newUsertest);
-  done();
+  it('Make new patient', done => {
+    socket_1.emit('signUp', newUsertest);
+    done();
   });
 
   //TEST check if subbed to finger prick
-  it('Make new patient',done =>
-  {
-  socket_1.emit('signUp',newUsertest);
-  done();
+  it('Make new patient', done => {
+    socket_1.emit('signUp', newUsertest);
+    done();
   });
 
   //TEST list all doctors
-  it('Make new patient',done =>
-  {
-  socket_1.emit('signUp',newUsertest);
-  done();
+  it('Make new patient', done => {
+    socket_1.emit('signUp', newUsertest);
+    done();
   });
 
 })

6e381ce8fa166653f26883314566ba0bcf2c5f3e
Zak Rooley <33326861+rooleyzee123@users.noreply.github.com>
2020-03-17 13:47:08 +0000

    mid way

---
+++ healthwebapp/src/components/authenticatedHeader.js
@@ -0,0 +1,83 @@
+const [bloodSugarModule, setBloodSugarModule] = React.useState(""); //stores the html section for the patient's blood sugar data
+React.useEffect(() => {
+    if (props.appProps.isDoctor) { //Checks if user requesting record is a patient or doctor
+        props.socket.emit("getMyPatientRecord", { selectedPatientID: props.appProps.currentSelectedPatient })//requests details for patient record from the back end server
+    }//if user is a doctor, we need to pass user ID of selected patient to the database when making the data request
+    else {
+        props.socket.emit("getMyPatientRecord", {});//requests details for patient record from the back end server
+    } //if user is a patient, the server  already knows their patient ID so we don't need to pass it to the server
+    props.socket.on("getMyPatientRecordResults", function (data) { //listener for patiient details information back from back end server
+        setBloodSugarModule(createBloodSugarModule(data.bloodSugarReadings)); //creates and stores html section for blood sugar section
+        return () => {
+            props.socket.off("getMyPatientRecordResults"); //turns off listener sockets for receiving data
+        }
+    });
+}, []);
+
+
+
+
+<Navbar bg="primary" expand="lg" variant="dark" role="navigation">
+<Image src={require('../images/logo.png')} alt="logo"></Image>
+<Navbar.Brand href="#">&nbsp; My Health Helper</Navbar.Brand>
+<Navbar.Toggle aria-controls="basic-navbar-nav" />
+<Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto" >
+
+        {!props.appProps.isDoctor ?
+            // Patient Navigation Bar
+            <Nav>
+                <LinkContainer to="/Patient/Homepage">
+                    <Nav.Link>Home</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/Patient/Food-Intake">
+                    <Nav.Link>Record Diet</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/Patient/Exercise">
+                    <Nav.Link>Record Exercise</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/Patient/MyDetails">
+                    <Nav.Link>My Details</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to='/register/Select-Doctor'>
+                    <Nav.Link>Change My Doctor</Nav.Link>
+                </LinkContainer>
+                {
+                    //Notification Section for User
+                }
+                <Dropdown as={ButtonGroup}>
+                    <Button type="button" className="NotificationButton" variant="light" className="pull-right">
+                        <span className="NotificationText">Notifications</span><span className="badge badge-danger ml-2">113</span>
+                    </Button>
+                    <Dropdown.Toggle split variant="light" id="dropdown-split-basic" />
+                    <Dropdown.Menu>
+                        {userNotifications()}
+                    </Dropdown.Menu></Dropdown>
+            </Nav>
+            : // Doctor Navigation Bar
+            <Nav>
+                <LinkContainer to="/HealthCareProfessional/Homepage">
+                    <Nav.Link>Home</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/HealthCareProfessional/My-Patient-List">
+                    <Nav.Link>My Patients</Nav.Link>
+                </LinkContainer>
+                {
+                    //Notification Section for Dr
+                }
+                <Dropdown as={ButtonGroup}>
+                    <Button type="button" className="NotificationButton" variant="light" >
+                        <span className="NotificationText">Notifications</span><span className="badge badge-danger ml-2">1</span>
+                    </Button>
+                    <Dropdown.Toggle split variant="light" id="dropdown-split-basic" />
+                    <Dropdown.Menu>
+                        {DrNotifications()}
+                    </Dropdown.Menu></Dropdown>
+            </Nav>
+        }
+    </Nav>
+</Navbar.Collapse>
+<Form inline>
+    <Button onClick={logOut} variant="outline-light">Log out</Button>
+</Form>
+</Navbar>
--- healthwebapp/src/components/header.js
+++ healthwebapp/src/components/header.js
@@ -4,11 +4,11 @@ import { Button, Form, Nav, Navbar, FormControl, NavItem, Dropdown, DropdownButt
 import { LinkContainer } from "react-router-bootstrap";
 import SocketContext from '../components/socket'
 import "../css/header.css";
+import authenticatedHeader from "./authenticatedHeader";
 
 
 export default function Header(props) {
     const [bloodSugarModule, setBloodSugarModule] = React.useState(""); //stores the html section for the patient's blood sugar data
-    if (props.appProps.isAuthenticated == true) {
         React.useEffect(() => {
             if (props.appProps.isDoctor) { //Checks if user requesting record is a patient or doctor
                 props.socket.emit("getMyPatientRecord", { selectedPatientID: props.appProps.currentSelectedPatient })//requests details for patient record from the back end server
@@ -23,7 +23,7 @@ export default function Header(props) {
                 }
             });
         }, []);
-    }
+    
 
     function logOut(e) {
         // Log user out and set local authenticated value to false
@@ -39,77 +39,10 @@ export default function Header(props) {
     }
     return (
         <>
+
             {props.appProps.isAuthenticated ? // Check if user is signed in
-                <Navbar bg="primary" expand="lg" variant="dark" role="navigation">
-                    <Image src={require('../images/logo.png')} alt="logo"></Image>
-                    <Navbar.Brand href="#">&nbsp; My Health Helper</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto" >
-
-                            {!props.appProps.isDoctor ?
-                                // Patient Navigation Bar
-                                <Nav>
-                                    <LinkContainer to="/Patient/Homepage">
-                                        <Nav.Link>Home</Nav.Link>
-                                    </LinkContainer>
-                                    <LinkContainer to="/Patient/Food-Intake">
-                                        <Nav.Link>Record Diet</Nav.Link>
-                                    </LinkContainer>
-                                    <LinkContainer to="/Patient/Exercise">
-                                        <Nav.Link>Record Exercise</Nav.Link>
-                                    </LinkContainer>
-                                    <LinkContainer to="/Patient/MyDetails">
-                                        <Nav.Link>My Details</Nav.Link>
-                                    </LinkContainer>
-                                    <LinkContainer to='/register/Select-Doctor'>
-                                        <Nav.Link>Change My Doctor</Nav.Link>
-                                    </LinkContainer>
-
-
-                                    {
-                                        //Notification Section for User
-                                    }
-
-                                    <Dropdown as={ButtonGroup}>
-                                        <Button type="button" className="NotificationButton" variant="light" className="pull-right">
-                                            <span className="NotificationText">Notifications</span><span className="badge badge-danger ml-2">113</span>
-                                        </Button>
-                                        <Dropdown.Toggle split variant="light" id="dropdown-split-basic" />
-                                        <Dropdown.Menu>
-                                            {userNotifications()}
-                                        </Dropdown.Menu></Dropdown>
-                                </Nav>
-                                : // Doctor Navigation Bar
-                                <Nav>
-                                    <LinkContainer to="/HealthCareProfessional/Homepage">
-                                        <Nav.Link>Home</Nav.Link>
-                                    </LinkContainer>
-                                    <LinkContainer to="/HealthCareProfessional/My-Patient-List">
-                                        <Nav.Link>My Patients</Nav.Link>
-                                    </LinkContainer>
-
-                                    {
-                                        //Notification Section for Dr
-                                    }
-                                    <Dropdown as={ButtonGroup}>
-                                        <Button type="button" className="NotificationButton" variant="light" >
-                                            <span className="NotificationText">Notifications</span><span className="badge badge-danger ml-2">1</span>
-                                        </Button>
-                                        <Dropdown.Toggle split variant="light" id="dropdown-split-basic" />
-                                        <Dropdown.Menu>
-
-                                            {DrNotifications()}
-                                        </Dropdown.Menu></Dropdown>
-                                </Nav>
-                            }
-                        </Nav>
-                    </Navbar.Collapse>
-                    <Form inline>
-                        <Button onClick={logOut} variant="outline-light">Log out</Button>
-                    </Form>
-                </Navbar>
-
+            
+            <authenticatedHeader/>
                 :
                 <>
                     {!props.appProps.registering ?
@@ -147,5 +80,4 @@ export default function Header(props) {
             }
         </>
     );
-
 }

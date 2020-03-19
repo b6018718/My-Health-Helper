15d1361d6192cbee2758f005cfdacd1388789d9a
Dan <danielrose@ntlword.com>
2020-03-19 14:10:45 +0000

    Update test-health-server.test.js

--- healthwebapp/tests/test-health-server.test.js
+++ healthwebapp/tests/test-health-server.test.js
@@ -135,180 +135,237 @@ describe("Web Health app Server", function () {
     socket_1.emit('signUp', newPatienttest);
   });
 
-  //TEST Add food 
-  it('Add food', done => {
-    socket_1.on('logInResult', function (data) {
-      var kitKat = {calories: 108, name:"KitKat", group:"Sugar"};
-      var food = [kitKat];
-      socket_1.emit("recordFoodDiary", food);
-      done();
-    });
-    socket_1.emit('logIn', PatientLogin);
-  });
+  // //TEST Add food 
+  // it('Add food', done => {
+  //   socket_1.on('logInResult', function (data) {
+  //     var kitKat = {calories: 108, name:"KitKat", group:"Sugar"};
+  //     var food = [kitKat];
+  //     socket_1.emit("recordFoodDiary", food);
+  //     done();
+  //   });
+  //   socket_1.emit('logIn', PatientLogin);
+  // });
 
-  //TEST add exersise
-  it('add exersise', done => {
-    socket_1.on('logInResult', function (data) {
-      var cycling = {exercisetype: "High", exercisename:"Cycling"};
-      socket_1.emit("recordExerciseDiary", [cycling]);
-      done();
-    });
-    socket_1.emit('logIn', PatientLogin);
-  });
+  // //TEST add exersise
+  // it('add exersise', done => {
+  //   socket_1.on('logInResult', function (data) {
+  //     var cycling = {exercisetype: "High", exercisename:"Cycling"};
+  //     socket_1.emit("recordExerciseDiary", [cycling]);
+  //     done();
+  //   });
+  //   socket_1.emit('logIn', PatientLogin);
+  // });
 
 
-  //TEST check blood sugar list
-  it('check blood sugar list', done => {
-    socket_1.on("getMyPatientRecordResults", async function(data){
-      if(data.bloodSugarReadings){
-        done();
+  // //TEST check blood sugar list
+  // it('check blood sugar list', done => {
+  //   socket_1.on("getMyPatientRecordResults", async function(data){
+  //     if(data.bloodSugarReadings){
+  //       done();
+  //     }
+  //   });
+
+  //   socket_1.on('logInResult', async function (data) {
+  //     socket_1.emit('getMyPatientRecord', {});
+  //   });
+
+  //   socket_1.emit('logIn', PatientLogin);
+  // });
+
+  // //TEST Get my doctor
+  // it('Get my doctor', done => {
+  //   var doctorId = "";
+  //   socket_1.on('getAllDoctorsResults', async function (data){
+  //     if(data.doctors.length > 0){
+  //       doctorId = data.doctors[0]._id;
+  //       socket_1.emit("updateAssignedDoctor", doctorId);
+  //       await sleep(100);
+  //       socket_1.emit("getMyDoctor", {});
+  //     }
+  //   });
+
+  //   socket_1.on('getMyDoctorResults', async function (data){
+  //     if(stringEquals(data.idAssignedDoctor, doctorId)){
+  //       done();
+  //     }
+  //   });
+
+  //   socket_1.on('logInResult', async function (data) {
+  //     socket_1.emit('getAllDoctors', {});
+  //   });
+
+  //   socket_1.emit('logIn', PatientLogin);
+  // });
+
+  // //TEST check if subbed to finger prick, negative case
+  // it('Check if finger prick subscribed', done => {
+  //   socket_1.on('checkIfSubscribedResults', async function (data) {
+  //     if (data.result) {
+  //       socket_1.emit("unSubscribeFingerPrick", {});
+  //       await sleep(100);
+  //       done();
+  //     }
+  //   });
+  //   socket_1.on('logInResult', async function (data) {
+  //     socket_1.emit("subscribeToFingerPrick", {});
+  //     await sleep(500);
+  //     socket_1.emit('checkIfSubscribed', {});
+  //   });
+  //   socket_1.emit('logIn', DrLogin);
+  // });
+
+  // //TEST check if subbed to finger prick, negative case
+  // it('Check if finger prick not subscribed', done => {
+  //   socket_1.on('checkIfSubscribedResults', function (data) {
+  //     if (!data.result) { done(); }
+  //   });
+  //   socket_1.on('logInResult', function (data) {
+  //     socket_1.emit('checkIfSubscribed', {});
+  //   });
+  //   socket_1.emit('logIn', DrLogin);
+  // });
+
+  // //TEST list all doctors
+  // it('List all doctors', done => {
+  //   socket_1.on('getAllDoctorsResults', function (data) {
+  //     if (data.doctors) { done(); }
+  //   });
+  //   socket_1.on('logInResult', function (data) {
+  //     socket_1.emit('getAllDoctors', {});
+  //   });
+  //   socket_1.emit('logIn', DrLogin);
+  // });
+
+  // //TEST Real time finger prick updates
+  // it('Real time finger prick updates for patient', done => {
+  //   socket_1.on('getMyPatientRecordResults', function (data) {
+  //     socket_1.emit("subscribeToFingerPrick", {});
+  //   });
+
+  //   socket_1.on("realTimeFingerPrickData", function (data){
+  //     socket_1.emit("unsubPatientRecord", {});
+  //     socket_1.emit("unSubscribeFingerPrick", {});
+  //     done();
+  //   });
+
+  //   socket_1.on('logInResult', function (data) {
+  //     socket_1.emit('getMyPatientRecord', {});
+  //   });
+  //   socket_1.emit('logIn', PatientLogin);
+  // });
+
+  // //TEST Real time food updates
+  // it('Real time food updates for patient', done => {
+  //   socket_1.on('getMyPatientRecordResults', function (data) {
+  //     var kitKat = {calories: 108, name:"KitKat", group:"Sugar"};
+  //     var food = [kitKat];
+  //     socket_1.emit("recordFoodDiary", food);
+  //   });
+
+  //   socket_1.on("realTimeFood", function (data){
+  //     done();
+  //   });
+
+  //   socket_1.on('logInResult', function (data) {
+  //     socket_1.emit('getMyPatientRecord', {});
+  //   });
+  //   socket_1.emit('logIn', PatientLogin);
+  // });
+
+  // // TEST Real time exercise updates
+  // it('Real time exercise updates for patient', done => {
+  //   socket_1.on('getMyPatientRecordResults', function (data) {
+  //     var cycling = {exercisetype: "High", exercisename:"Cycling"};
+  //     socket_1.emit("recordExerciseDiary", [cycling]);
+  //   });
+
+  //   socket_1.on("realTimeExercise", function (data){
+  //     done();
+  //   });
+
+  //   socket_1.on('logInResult', function (data) {
+  //     socket_1.emit('getMyPatientRecord', {});
+  //   });
+  //   socket_1.emit('logIn', PatientLogin);
+  // });
+
+  
+  /////////////////// Task 3 tests //////////////
+  
+  //TEST doctor send request
+  it('Send request', done => {
+    var patientId = "";
+    socket_1.on('getAllPatientsResults', async function (data){
+      if(data.patients.length > 0){
+        patientId = data.patients[0]._id;
+        socket_1.emit("sendDoctorRequest", patientId);
       }
     });
-
+    
+    socket_1.on('sendDoctorRequestResult', function(data)  {
+      if (data == "Success!") { done(); }
+    })
+    
     socket_1.on('logInResult', async function (data) {
-      socket_1.emit('getMyPatientRecord', {});
-    });
-
-    socket_1.emit('logIn', PatientLogin);
-  });
-
-  //TEST Get my doctor
-  it('Get my doctor', done => {
-    var doctorId = "";
-    socket_1.on('getAllDoctorsResults', async function (data){
-      if(data.doctors.length > 0){
-        doctorId = data.doctors[0]._id;
-        socket_1.emit("updateAssignedDoctor", doctorId);
-        await sleep(100);
-        socket_1.emit("getMyDoctor", {});
-      }
-    });
-
-    socket_1.on('getMyDoctorResults', async function (data){
-      if(stringEquals(data.idAssignedDoctor, doctorId)){
-        done();
-      }
-    });
-
-    socket_1.on('logInResult', async function (data) {
-      socket_1.emit('getAllDoctors', {});
-    });
-
-    socket_1.emit('logIn', PatientLogin);
-  });
-
-  //TEST check if subbed to finger prick, negative case
-  it('Check if finger prick subscribed', done => {
-    socket_1.on('checkIfSubscribedResults', async function (data) {
-      if (data.result) {
-        socket_1.emit("unSubscribeFingerPrick", {});
-        await sleep(100);
-        done();
-      }
-    });
-    socket_1.on('logInResult', async function (data) {
-      socket_1.emit("subscribeToFingerPrick", {});
-      await sleep(500);
-      socket_1.emit('checkIfSubscribed', {});
-    });
-    socket_1.emit('logIn', DrLogin);
-  });
-
-  //TEST check if subbed to finger prick, negative case
-  it('Check if finger prick not subscribed', done => {
-    socket_1.on('checkIfSubscribedResults', function (data) {
-      if (!data.result) { done(); }
-    });
-    socket_1.on('logInResult', function (data) {
-      socket_1.emit('checkIfSubscribed', {});
-    });
-    socket_1.emit('logIn', DrLogin);
-  });
-
-  //TEST list all doctors
-  it('List all doctors', done => {
-    socket_1.on('getAllDoctorsResults', function (data) {
-      if (data.doctors) { done(); }
-    });
-    socket_1.on('logInResult', function (data) {
-      socket_1.emit('getAllDoctors', {});
-    });
-    socket_1.emit('logIn', DrLogin);
-  });
-
-  //TEST Real time finger prick updates
-  it('Real time finger prick updates for patient', done => {
-    socket_1.on('getMyPatientRecordResults', function (data) {
-      socket_1.emit("subscribeToFingerPrick", {});
-    });
-
-    socket_1.on("realTimeFingerPrickData", function (data){
-      socket_1.emit("unsubPatientRecord", {});
-      socket_1.emit("unSubscribeFingerPrick", {});
-      done();
-    });
-
-    socket_1.on('logInResult', function (data) {
-      socket_1.emit('getMyPatientRecord', {});
-    });
-    socket_1.emit('logIn', PatientLogin);
-  });
-
-  //TEST Real time food updates
-  it('Real time food updates for patient', done => {
-    socket_1.on('getMyPatientRecordResults', function (data) {
-      var kitKat = {calories: 108, name:"KitKat", group:"Sugar"};
-      var food = [kitKat];
-      socket_1.emit("recordFoodDiary", food);
-    });
-
-    socket_1.on("realTimeFood", function (data){
-      done();
-    });
-
-    socket_1.on('logInResult', function (data) {
-      socket_1.emit('getMyPatientRecord', {});
-    });
-    socket_1.emit('logIn', PatientLogin);
-  });
-
-  // TEST Real time exercise updates
-  it('Real time exercise updates for patient', done => {
-    socket_1.on('getMyPatientRecordResults', function (data) {
-      var cycling = {exercisetype: "High", exercisename:"Cycling"};
-      socket_1.emit("recordExerciseDiary", [cycling]);
-    });
-
-    socket_1.on("realTimeExercise", function (data){
-      done();
-    });
-
-    socket_1.on('logInResult', function (data) {
-      socket_1.emit('getMyPatientRecord', {});
-    });
-    socket_1.emit('logIn', PatientLogin);
-  });
-
-  //TEST remove doctor account
-  it('Remove Doctor account', done => {
-    socket_1.on('deleteAccountResults', function (data) {
-      if (data == "Success") { done(); }
+      socket_1.emit('getAllPatients', {});
     })
-    socket_1.emit('logIn', DrLogin);
-    socket_1.on('logInResult', function (data) {
-      socket_1.emit('deleteAccount', {});
-    })
-  });
+  socket_1.emit('logIn', DrLogin);
+});
 
-  //TEST remove Patient account
-  it('Remove Patient account', done => {
-    socket_1.on('deleteAccountResults', function (data) {
-      if (data == "Success") { done(); }
-    })
-    socket_1.emit('logIn', PatientLogin);
-    socket_1.on('logInResult', function (data) {
-      socket_1.emit('deleteAccount', {});
-    })
-  });
+it('Cancel Request', done => {
+  socket_1.emit('logIn', PatientLogin);
+  socket_1.emit('cancelDoctorRequest', {});
+  socket_1.on('cancelDoctorRequestResult', function(data)  {
+    if (data == "Success!") { done(); }
+  })
 })
 
+it('Accept request', done => {
+  var patientId = "";
+  socket_1.on('getAllPatientsResults', async function (data){
+    if(data.patients.length > 0){
+      patientId = data.patients[0]._id;
+      socket_1.emit("sendDoctorRequest", patientId);
+    }
+  });
+  
+  socket_1.on('sendDoctorRequestResult', function(data)  {
+    socket_1.emit('logIn', PatientLogin);
+    socket_1.emit('acceptDoctorRequest', {});
+    socket_1.on('acceptDoctorRequestResult', function(data)  {
+      if (data == "Success!") { done(); }
+    })
+  })
+  
+  socket_1.on('logInResult', async function (data) {
+    socket_1.emit('getAllPatients', {});
+  })
+socket_1.emit('logIn', DrLogin);
+});
+
+
+
+
+
+//TEST remove doctor account
+it('Remove Doctor account', done => {
+  socket_1.on('deleteAccountResults', function (data) {
+    if (data == "Success") { done(); }
+  })
+  socket_1.emit('logIn', DrLogin);
+  socket_1.on('logInResult', function (data) {
+    socket_1.emit('deleteAccount', {});
+  })
+});
+
+//TEST remove Patient account
+it('Remove Patient account', done => {
+  socket_1.on('deleteAccountResults', function (data) {
+    if (data == "Success") { done(); }
+  })
+  socket_1.emit('logIn', PatientLogin);
+  socket_1.on('logInResult', function (data) {
+    socket_1.emit('deleteAccount', {});
+  })
+});
+})
